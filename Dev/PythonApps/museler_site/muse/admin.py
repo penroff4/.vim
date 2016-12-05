7b790dd25c0e389ce64c7d15aa1a6e0f7a68703d
penroff4 <penroff4@yahoo.com>
2016-12-04 20:42:31 -0800

    Remove GooUser model, rename PlaylistUser

--- Dev/PythonApps/museler_site/muse/admin.py
+++ Dev/PythonApps/museler_site/muse/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import GooUser
+from .models import Channel, Video, Playlist, PlaylistVideo 
 
-admin.site.register(GooUser)
-
+admin.site.register(Channel)
+admin.site.register(Video)
+admin.site.register(Playlist)
+admin.site.register(PlaylistVideo)
