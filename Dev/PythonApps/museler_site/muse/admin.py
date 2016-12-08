09cea5055e30e237cc16b29a067a7fc764dc5ad7
penroff4 <penroff4@yahoo.com>
2016-12-08 16:04:26 -0600

    remove dev/pythonapps/museler_site

--- Dev/PythonApps/museler_site/muse/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Channel, Video, Playlist, PlaylistVideo 
-
-admin.site.register(Channel)
-admin.site.register(Video)
-admin.site.register(Playlist)
-admin.site.register(PlaylistVideo)
